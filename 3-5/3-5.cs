5ca8eb5bbe6641726b3dc995e5618ef8172f3685
qqq991124 <qqq991124@naver.com>
2023-03-21 18:12:01 +0900

    Commit Message

--- 3-5/3-5.cs
+++ 3-5/3-5.cs
@@ -7,10 +7,18 @@ namespace _3_5
         static void Main(string[] args)
         {
             Console.Write("임의의 정수 n을 입력하시오: ");
-            int input = int.Parse(Console.ReadLine());
-
-            if (input == 0)
-                Console.Write("팩토리얼 0의 값: 1");
+            int input;
+            while ((input = int.Parse(Console.ReadLine())) < 0)
+            {
+                Console.WriteLine("Error: 유효하지 않은 범위입니다. 다시 입력하세요. ");
+                Console.Write("임의의 정수 n을 입력하시오: ");
+            }
+            
+            if (input < 2)
+            {
+                Console.Write("팩토리얼 " + input + "의 값: 1");
+                return;
+            }
 
             int[] ans = new int[input + 1];
             ans[0] = 1;
